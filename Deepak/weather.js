690fdbfd3694ef9a5798d83af616edd97cc7bd01
deepakstar04 <dk134deepakstar@gmail.com>
2025-09-13 14:36:22 +0530

    ADD

--- Deepak/weather.js
+++ Deepak/weather.js
@@ -1,21 +1,28 @@
-const apiKey ="d63098f145215262649c95ee84989361";
+const apiKey = "d63098f145215262649c95ee84989361";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const searchBox = document.querySelector(".search input")
-const searchbtn = document.querySelector(".search button")
-async function checkweather(city){
-    const response = await fetch(apiUrl + city`&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkweather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
 
+    if (response.status == 404) {
+        document.querySelector(".weather").style.display = "none";
+        alert("City not found. Please enter a valid city name.");
+    } else {
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        document.querySelector(".weather").style.display = "block";
+    }
 }
 
-
- document.querySelector("city").innerHTML = data.name;
- document.querySelector("temp").innerHTML = Math.round(data.main.temp) + "°c";
- document.querySelector("humidity").innerHTML = data.main.humidity + "%";
- document.querySelector("wind").innerHTML = data.main.woind.speed + "km/h";
- 
- searchbtn.addEventListener("click",()=>{
+searchbtn.addEventListener("click", () => {
     checkweather(searchBox.value);
- })
+})
